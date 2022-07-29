a61ce471055cadefc22c32b6864d9dca2c9f40ac
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-07-29 17:18:49 -0300

    Realizei o requisito 2

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,27 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+    editor: false,
+    idToEdit: 0,
+  },
+};
+
+function userReducers(state = initialState, action) {
+  switch (action.type) {
+  case 'NEW_WALLET':
+    return {
+      wallet: {
+        currencies: [],
+        expenses: [],
+        editor: false,
+        idToEdit: 0,
+      },
+    };
+  default:
+    return state;
+  }
+}
+
+export default userReducers;
