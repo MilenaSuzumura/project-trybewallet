978a2f8d66d05aab469e34a7dc0d3d06c8c9cebe
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-07-29 16:56:02 -0300

    requisito 1 realizado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet! </div>;
+  return (
+    <div>
+      Hello, TrybeWallet!
+      <Route path="/" component={ Login } exact />
+      <Route path="/carteira" component={ Wallet } />
+    </div>
+  );
 }
 
 export default App;
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  user: {},
+};
+
+function userReducers(state = initialState, action) {
+  switch (action.type) {
+  case 'NEW_USER':
+    return {
+      email: action.email,
+    };
+  default:
+    return state;
+  }
+}
+
+export default userReducers;
